efe93322a857f6a62ded7ceb0d9044d12ac28c7c
OutumnRay <152505344+OutumnRay@users.noreply.github.com>
2025-09-06 15:24:25 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -140,36 +140,4 @@ int main(int argc, char **argv) {
     return RUN_ALL_TESTS();
 }
 
-int main() {
-    std::vector<Student> database;
 
-    int choice;
-    do {
-        std::cout << "Меню:\n";
-        std::cout << "1. Добавить студента\n";
-        std::cout << "2. Вывести список студентов\n";
-        std::cout << "3. Найти студента с наименьшим и наибольшим баллом\n";
-        std::cout << "0. Выход\n";
-        std::cout << "Выберите действие: ";
-        std::cin >> choice;
-
-        switch (choice) {
-            case 1:
-                addStudent(database);
-                break;
-            case 2:
-                displayStudents(database);
-                break;
-            case 3:
-                findMinMaxGPA(database);
-                break;
-            case 0:
-                std::cout << "Выход из программы.\n";
-                break;
-            default:
-                std::cout << "Неверный выбор. Попробуйте снова.\n";
-        }
-    } while (choice != 0);
-
-    return 0;
-}
